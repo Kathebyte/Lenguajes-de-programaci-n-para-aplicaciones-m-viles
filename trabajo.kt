a1638b9fce6ae2b3dbedd75fe7b7e98e5bbe9bdc
Katherine Benjumea <kathe07338@gmail.com>
2026-03-01 17:28:06 -0500

    creacion arreglos, calculo promedio

--- trabajo.kt
+++ trabajo.kt
@@ -1,17 +1,46 @@
 fun main() {
-    println("Hola Mundo")
-    val nombre = pedirNombre()
+    println("Lenguajes de programación para aplicaciones móviles")
+    val nombre = nombreEstudiante()
     println("Hola $nombre")
-    val numeroEstudiantes = pedirCantidadEstudiantes()
+
+    // pedir numero de estudiantes
+    val numeroEstudiantes = totalCantidadEstudiantes()
     println("Hay $numeroEstudiantes estudiantes en el aula")
+
+    val nombres = Array(numeroEstudiantes) { "" }
+    val finales = Array(numeroEstudiantes) { 0.0 }
+
+    for (i in 0 until numeroEstudiantes) {
+        println("\n Informacion del Estudiante ${i + 1} ")
+
+        print("Nombre Completo: ")
+        nombres[i] = readln() // Lee el nombre como texto
+
+        // Pedimos las 3 notas
+        print("Nota 1: ")
+        val n1 = readln().toDouble()
+
+        print("Nota 2: ")
+        val n2 = readln().toDouble()
+
+        print("Nota 3: ")
+        val n3 = readln().toDouble()
+
+        var finalNota = calcularPromedio(n1, n2, n3)
+        finales[i] = finalNota
+    }
 }
 
-fun pedirCantidadEstudiantes(): Int {
+fun totalCantidadEstudiantes(): Int {
     println("¿Cuántos estudiantes hay en el aula?")
     return readln().toInt()
 }
 
-fun pedirNombre(): String {
+fun nombreEstudiante(): String {
     println("¿Cuál es tu nombre?")
     return readln()
+}
+
+fun calcularPromedio(n1: Double, n2: Double, n3: Double): Double {
+    return (n1 + n2 + n3) / 3
 }
